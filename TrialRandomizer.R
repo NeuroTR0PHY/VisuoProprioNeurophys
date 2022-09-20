c80e2a7574b0909ed47525bbd7aa326619e370a7
NeuroTR0PHY <neurotrophy@gmail.com>
2022-09-20 12:05:58 -0400

    Update TrialRandomizer.R

---
+++ TrialRandomizer.R
@@ -0,0 +1,19 @@
+trials = 16
+randomized = sample(1:trials)
+randomized
+
+
+randomized = sample(seq(10,80,by = 10))
+randomized
+
+
+x = rep("Index",6)
+y = rep("Thenar", 6)
+z = c(x,y)
+randomized = sample(z)
+randomized
+
+x = rep("Left",3)
+y = rep("Right",3)
+z = c(x,y)
+sample(z)
